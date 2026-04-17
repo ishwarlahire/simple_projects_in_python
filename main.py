41b452cf7b6c30b6cd885ae65e30742139a76448
ishwarlahire <ishwarlahire2004@gmail.com>
2026-04-17 19:02:11 +0530

    create a simple calculator in python using while loop

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-def main():
+while True:
     num1 = int(input("Enter First Number"))
     num2 = int(input("Enter Second Number"))
-    task = int(input("Please select one option \n for Addition press 1 \n for Subtraction press 2 \n for multiplication press 3 \n for division press 4 \n"))
-    return num1, num2, task
-
-def add(num1,num2):
-    return num1+num2
-def sub(num1,num2):
-    return num1-num2
-def mul(num1,num2):
-    return num1*num2
-def div(num1,num2):
-    return num1/num2
-
-num1, num2, task = main()
-
-if task == 1:
-    print(add(num1,num2))
-elif task == 2:
-    print(sub(num1,num2))
-elif task == 3:
-    print(mul(num1,num2))
-elif task == 4:
-    print(div(num1,num2))
-else :
-    print("Enter correct option")
-    
+    task = int(input("Please select one option \n for Addition press 1 \n for Subtraction press 2 \n for multiplication press 3 \n for division press 4 \n for exit form calculator enter 5\n"))
+    if task == 1:
+        print((num1 + num2))
+    elif task == 2:
+        print((num1 - num2))
+    elif task == 3:
+        print((num1 * num2))
+    elif task == 4:
+        print((num1 / num2))
+    elif task == 5:
+        print("thank you for used my calcutor")
+        break
+    else :
+        print("Enter correct option")
