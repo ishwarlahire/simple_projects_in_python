d54364b3e8abdb3830307c6d5f53ce536feabf05
ishwarlahire <ishwarlahire2004@gmail.com>
2026-05-21 16:45:32 +0530

    LEARN MORE CONCEPTS and practice it

---
+++ string_methods.py
@@ -0,0 +1,15 @@
+intro = "My name is Ishu!"
+
+print(intro[0]) 
+print(intro[0:2])
+print(intro[-5:-1])
+
+print(len(intro)) 
+
+intro.lower()
+intro.upper()
+intro.title()
+
+print(intro.split())
+print(intro.split('name')) 
+print(intro.split('!'))
